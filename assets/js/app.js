42e3a6e3b8ec928aa79e56993faf5d696ee8308e
Timothé Duc <timotheduc@gmail.com>
2019-09-19 17:52:38 +0100

    full responsive website

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,13 +9,15 @@
 require('../css/app.scss');
 require('bootstrap');
 require('flickity');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
 
 $(function () {
 	$(document).scroll(function () {
-		var $nav = $(".navbar");
+		const $nav = $(".navbar");
 		$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
 });
